2029dd38e1c63077db419135949a350bcca3372b
renan moura <renan.barros.dev@gmail.com>
2023-08-26 14:07:50 -0300

    chore: test config and packages

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testTimeout: 180000
+  testTimeout: 180000,
+  globalSetup: "<rootDir>/test/globalSetup.ts",
+  globalTeardown: "<rootDir>/test/globalTeardown.ts",
+  setupFilesAfterEnv: [
+    "<rootDir>/test/setupFile.ts"
+  ]
 };
