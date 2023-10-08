eed85c06c22353dac051550b0e29dd792326d2c8
renan moura <renan.barros.dev@gmail.com>
2023-10-08 07:56:10 -0300

    update

--- src/presentation/scripts/genetateTranslate.ts
+++ src/presentation/scripts/genetateTranslate.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-dotenv.config({ path: ".env" });
+dotenv.config({ path: "../../../.env" });
 import MongoDB from "../../infrastructure/repositories/mongodb/mongodb";
 import { Translate } from "../../application/usecases/main/Translate";
 import { languages } from "./languages";
